967df9d73c7d109fe5574f9c877d535089666809
Mauricio Orozco <andres.mauricio.orozco@gmail.com>
2022-09-09 15:07:24 -0500

    segundo commit

---
+++ Taller.App/Taller.App.Front/Pages/AgendarCita/webpack.js
@@ -0,0 +1,10 @@
+import img1 from './img/seguro-de-auto.jpg'
+import img2 from './img/uno.png'
+
+
+function Header() {
+  // Import result is the URL of your image
+  return <img src={logo} alt="Logo" />;
+}
+
+export default Header;
